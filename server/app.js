d2a07397e0410092d842c8ea4dcef88a529c9c84
parksh100 <80499983+parksh100@users.noreply.github.com>
2022-09-15 18:11:11 +0900

    서버구성진행중

--- server/app.js
+++ server/app.js
@@ -4,7 +4,14 @@ const app = express();
 const port = 3000;
 require("dotenv").config({ path: "mysql/.env" }); // 반드시 mysql위에 있어야 함.
 const mysql = require("./mysql");
+const multer = require("multer");
+const path = require("path");
 
+//----------------------------------------------------
+app.use(express.static("public")); // 서버에서 이미지를 다운받아야 할때 사용. static("열어줄 폴더")
+// 브라우저에 localhost:3000//images/q8.jpg 입력하면 이미지 바로 볼 수 있음.
+
+// ---------------------------------------------------
 // 클라이언트 요청 body를 json으로 파싱 처리
 // post로 실러오는 데이터를 받을 때 반드시 필요.
 app.use(
@@ -13,14 +20,53 @@ app.use(
   })
 );
 
+// multer --------------------------------------------------
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // 어떤폴더에 저장
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    // 시스템 시간으로 파일이름 변경
+    cb(null, new Date().valueOf() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/attachment", upload.single("attachment"), async (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+
+  const fileInfo = {
+    auditor_id: parseInt(req.body.auditor_id),
+    originalname: req.file.originalname,
+    mimetype: req.file.mimetype,
+    filename: req.file.filename,
+    path: req.file.path,
+  };
+  res.send(fileInfo);
+  // const r = await mysql.query("auditorImage", fileInfo);
+  // res.send(r);
+});
+
+//웹서버 시작--------------------------------------------------------
 app.listen(port, () => {
   console.log(`Server start on port ${port}!!`);
 });
 
-//auditor 조회
+//라우트 동록------------------------------------------------------------
+//auditor 전체조회
 app.get("/api/auditor", async (req, res) => {
   const auditorList = await mysql.query("auditorList");
   res.send(auditorList);
+});
+
+//auditor 한 명조회
+app.get("/api/auditor/:auditor_id", async (req, res) => {
+  const { auditor_id } = req.params;
+  const getAuditor = await mysql.query("getAuditor", auditor_id);
+  res.send(getAuditor);
 });
 
 // auditor 생성
@@ -45,3 +91,11 @@ app.delete("/api/auditor/:auditor_id", async (req, res) => {
   const result = await mysql.query("auditorDelete", auditor_id);
   res.send(result);
 });
+
+/*
+// route alias 실무에서는 번거로우니까 이렇게 사용함.
+app.post("/api/:alias", async (req, res) => {
+  const auditorList = await mysql.query(req.params.alias, req.body.param);
+  res.send(auditorList);
+});
+*/
--- server/mysql/sql.js
+++ server/mysql/sql.js
@@ -1,6 +1,8 @@
 module.exports = {
   auditorList: `select * from auditor`,
   auditorInsert: `insert into auditor set ?`,
+  auditorImage: `insert into auditor_image set ?`,
   auditorUpdate: `update auditor set ? where auditor_id=?`,
   auditorDelete: `delete from auditor where auditor_id=?`,
+  getAuditor: `select * from auditor where auditor_id=?`,
 };
